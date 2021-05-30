af4722f824a29192163b26088e6b9180ca90a13b
yueymiqua <pierre1116@gmail.com>
2021-05-30 11:01:04 -0400

    created landingpage and pascalstriangle component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+
+import PascalsTriangle from './components/pascals-triangle.component';
+import LandingPage from './components/landing-page.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
     </div>
   );
 }
---
+++ src/components/pascals-triangle.component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PascalsTriangle = () => {
+  return(
+    <div>This is the Pascals Triangle Page</div>
+  )
+}
+
+export default PascalsTriangle;

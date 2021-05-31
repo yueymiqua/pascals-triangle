4ea664d49fabeb8eff52ddaf812fa76cd41f0ff9
yueymiqua <pierre1116@gmail.com>
2021-05-30 22:09:49 -0400

    added pascals-triangle.component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,78 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import PascalsTriangle from './components/pascals-triangle.component';
-import LandingPage from './components/landing-page.component';
 
 function App() {
+  
+  const [numOfRows, setNumOfRows] = useState(0);
+  
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <Router>
+      <Route path='/' exact>
+        <div className="container">
+          <h3>Select Number for Pascal Triangle</h3>
+          <table className="table">
+            <tr>
+              <td>
+                <Link to={'/pascalstriangle'}>
+                  <button className="btn btn-secondary" onClick={() => setNumOfRows(1)}>1</button>
+                </Link>
+              </td>
+              <td>
+                <Link to={'/pascalstriangle'}>
+                  <button className="btn btn-secondary" onClick={() => setNumOfRows(2)}>2</button>
+                </Link>
+              </td>
+              <td>
+                <Link to={'/pascalstriangle'}>
+                  <button className="btn btn-secondary" onClick={() => setNumOfRows(3)}>3</button>
+                </Link>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={'/pascalstriangle'}>
+                  <button className="btn btn-secondary" onClick={() => setNumOfRows(4)}>4</button>
+                </Link>
+              </td>
+              <td>
+                <Link to={'/pascalstriangle'}>
+                  <button className="btn btn-secondary" onClick={() => setNumOfRows(5)}>5</button>
+                </Link>
+              </td>
+              <td>
+                <Link to={'/pascalstriangle'}>
+                  <button className="btn btn-secondary" onClick={() => setNumOfRows(6)}>6</button>
+                </Link>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Link to={'/pascalstriangle'}>
+                  <button className="btn btn-secondary" onClick={() => setNumOfRows(7)}>7</button>
+                </Link>
+              </td>
+              <td>
+                <Link to={'/pascalstriangle'}>
+                  <button className="btn btn-secondary" onClick={() => setNumOfRows(8)}>8</button>
+                </Link>
+              </td>
+              <td>
+                <Link to={'/pascalstriangle'}>
+                  <button className="btn btn-secondary" onClick={() => setNumOfRows(9)}>9</button>
+                </Link>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </Route>
+      <Route path='/pascalstriangle' exact>
+        <PascalsTriangle numOfRows={numOfRows}/>
+      </Route>
+    </Router>
   );
 }
 
--- src/components/pascals-triangle.component.js
+++ src/components/pascals-triangle.component.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 
-const PascalsTriangle = () => {
-  return(
-    <div>This is the Pascals Triangle Page</div>
+const PascalsTriangle = (props) => {
+
+  const [currentRow, setCurrentRow] = useState(0);
+  const [prevRow, setPrevRow] = useState(0);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(props.numOfRows);
+    console.log(counter);
+  },[counter]);
+
+  return (
+    <div>
+      <div>{props.numOfRows}</div>
+      <Link to='/'>
+        <button className="btn btn-primary">Back to Home Page</button>
+      </Link>
+    </div>
   )
 }
 
